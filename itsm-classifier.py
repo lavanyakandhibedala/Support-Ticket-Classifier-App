1f42145981998c2ab1b251e5c39570605f460759
Lavanya Kandhibedala <kandhibedalalavanya@yahoo.com>
2021-02-04 12:32:05 +0530

    Update itsm-classifier.py

--- itsm-classifier.py
+++ itsm-classifier.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 # %matplotlib inline
-import seaborn as sbn
+import seaborn as sns
 import math
 import nltk
 from sklearn.model_selection import train_test_split
